b4e23c2b7526e6d3fae199d8c37fa9ce92d90c7f
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-09-23 16:56:44 -0500

    muae_from_times: pass through second return value of epoch_from_times
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- muae_from_times.m
+++ muae_from_times.m
@@ -1,7 +1,7 @@
-function [muae_zs] = muae_from_times(probe_signal,trial_times,baseline)
+function [muae_zs, times] = muae_from_times(probe_signal,trial_times,baseline)
 %MUAE_FROM_TIMES Summary of this function goes here
 %   Detailed explanation goes here
-muae = epoch_from_times(probe_signal, trial_times);
+[muae, times] = epoch_from_times(probe_signal, trial_times);
 freq = probe_signal.starting_time_rate;
 muae_bs = baseline_correct(muae, 1:(baseline * freq));
 muae_zs = permute(trial_zscore(permute(muae_bs, [1, 3, 2])), [1, 3, 2]);
