9a49b4b7b2e75ee5a41dd01ba25dd82b6f969eb8
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-09-23 16:57:09 -0500

    heatmap_muae: plot xlines for all stimuli, not second and onward
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- heatmap_muae.m
+++ heatmap_muae.m
@@ -5,5 +5,5 @@ trials = 1:size(muae_zs, 3);
 figure;
 im = imagesc('XData', times_in_trial, 'YData', trials, 'CData', transpose(squeeze(mean(muae_zs, 1))));
 colormap("parula"); colorbar; clim([-1, 1]); xlim([-inf, inf]); ylim([-inf, inf]);
-xline(stim_times(2:end), '-'); set(gca,'YDir','reverse');
+xline(stim_times, '-'); set(gca,'YDir','reverse');
 end
