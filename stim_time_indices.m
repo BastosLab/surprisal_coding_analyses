2cc72f799d458b073a342fdd90bdb1b703f24ed5
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-09-24 14:18:30 -0500

    stim_time_indices.m: don't slice signal timestamps by stimulus number
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- stim_time_indices.m
+++ stim_time_indices.m
@@ -3,8 +3,8 @@ function [indices] = stim_time_indices(times, stim_times)
 %   Detailed explanation goes here
 indices = [];
 for s=1:size(stim_times, 1)
-    start_idx = nearest_index(squeeze(times(1, :, s)), stim_times(s, 1));
-    end_idx = nearest_index(squeeze(times(1, :, s)), stim_times(s, 2));
+    start_idx = nearest_index(squeeze(times(1, :)), stim_times(s, 1));
+    end_idx = nearest_index(squeeze(times(1, :)), stim_times(s, 2));
     idx = cat(2, start_idx, end_idx);
     indices = cat(1, indices, idx);
 end
