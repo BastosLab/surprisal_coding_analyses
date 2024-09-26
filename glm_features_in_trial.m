f64cb050d1f550b7c3e53cd3d9a019f060f088a9
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-09-26 14:58:32 -0500

    glm_features_in_trial.m: average over channels, take MUA and features over time
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- glm_features_in_trial.m
+++ glm_features_in_trial.m
@@ -1,24 +1,27 @@
 function [stim_muae_zs, predictors] = glm_features_in_trial(muae_zs, times_in_trial, stims_in_trial)
-%GLM_FEATURES Summary of this function goes here
-%   Detailed explanation goes here
-nchannels = size(muae_zs, 1);
-nstims = size(stims_in_trial, 1);
+%GLM_FEATURES
+% stim_muae_zs: Stimuli x Samples in Time x Trials
+% predictors: Stimuli x Samples in Time x Trials x Features
 ntrials = size(muae_zs, 3);
+nstims = size(stims_in_trial, 1);
 stim_indices = stim_time_indices(times_in_trial, stims_in_trial);
+ntimes = round(mean(stim_indices(:, 2) - stim_indices(:, 1)));
 
-stim_muae_zs = nan(nstims, nchannels, ntrials);
-predictors = nan(nstims, 3 + nstims, ntrials);
+stim_muae_zs = nan(nstims, ntimes, ntrials);
+predictors = nan(nstims, ntimes, ntrials, 4);
 for s=1:nstims
-    stim_timeslices = muae_zs(:, stim_indices(s, 1):stim_indices(s, 2), :);
-    stim_muae_zs(s, :, :) = squeeze(mean(stim_timeslices, 2));
+    last_idx = stim_indices(s, 1) + ntimes - 1;
+    stim_muae_zs(s, :, :) = mean(muae_zs(:, stim_indices(s, 1):last_idx, :), 1);
 
-    predictor = zeros(3 + nstims, ntrials);
-    predictor(s, :) = 1;
-    predictor(end-2, :) = 1:ntrials;
-    predictor(end-1, :) = stims_in_trial(s, 1);
-    predictor(end, :) = s;
+    predictor = zeros(ntrials, 3);
+    predictor(:, 1) = 1:ntrials;
+    predictor(:, 2) = squeeze(mean(stims_in_trial(s, :), 2));
+    predictor(:, 3) = s;
+    predictor = repmat(permute(predictor, [4, 3, 1, 2]), [1, ntimes, 1, 1]);
 
-    predictors(s, :, :) = predictor;
+    stim_times_in_trial = permute(times_in_trial(:, stim_indices(s, 1):last_idx), [3, 1, 2]);
+    predictors(s, :, :, 1) = repmat(stim_times_in_trial, [1, 1, 1, ntrials]);
+    predictors(s, :, :, 2:end) = predictor;
 end
 end
 
