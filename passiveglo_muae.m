8b2394967e7f1326ad89e09c35154b5b6d38ee85
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-09-26 14:45:09 -0500

    passiveglo_muae: take trial and stimulus times as input to avoid repetition
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- passiveglo_muae.m
+++ passiveglo_muae.m
@@ -1,8 +1,9 @@
-function [muae_zs, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, intervals_mask, baseline)
-%CHANNEL_TIME_FEATURES Summary of this function goes here
-%   Detailed explanation goes here
+function [muae_zs, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, baseline)
+%CHANNEL_TIME_FEATURES
+%muaez_zs: Channels x Samples in Time x Trials
+%times_in_trial: 1 x Samples in Time
+%stims_in_trial: Stimuli x 2 Times
 
-[trials, stims] = passiveglo_block_times(nwb, intervals_mask);
 stims_in_trial = epoch_stimtimes(trials, stims);
 probe0_data = nwb.acquisition.get('probe_0_muae').electricalseries.get('probe_0_muae_data');
 [muae_zs, times_in_trial] = muae_from_times(probe0_data, trials, baseline);
