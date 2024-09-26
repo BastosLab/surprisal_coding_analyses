830ec7437622b2034b4b4e1978a3cef34dee9cdc
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-09-26 17:14:10 -0500

    glm_features.m: offset to capture fixation, include pupil diameter as feature
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- glm_features.m
+++ glm_features.m
@@ -1,14 +1,13 @@
-function [stim_muae_zs, predictors] = glm_features(nwb, intervals_mask, baseline)
+function [stim_muae_zs, predictors] = glm_features(nwb, intervals_mask, baseline, offset)
 %GLM_FEATURES
 %stim_muae_zs: Stimuli x Samples in Time x Trials
 %predictors: Stimuli x Samples in Time x Trials x Features
 [trials, stims, stim_info] = passiveglo_block_times(nwb, intervals_mask);
-rewards = epoch_signal(nwb, 'reward', trials);
-pupils = epoch_signal(nwb, 'pupil', trials);
-pupils = squeeze(mean(pupils(:, 1:500, :), 2))';
+trials(:, 1) = trials(:, 1) - offset;
+trials(:, 2) = trials(:, 2) + offset;
+pupils = time_zscore(epoch_signal(nwb, 'pupil', trials));
 [muae_zs, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, baseline);
-[stim_muae_zs, predictors] = glm_features_in_trial(muae_zs, times_in_trial, stims_in_trial);
+[stim_muae_zs, predictors] = glm_features_in_trial(muae_zs, pupils, times_in_trial, stims_in_trial);
 stim_info = cat(3, stims, stim_info);
 predictors = cat(4, predictors, repmat(permute(stim_info, [2, 4, 1, 3]), [1, size(predictors, 2), 1, 1]));
 end
-
