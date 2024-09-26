4562b920fb428232eabf3ff78f235c9d3e51f314
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-09-26 17:13:40 -0500

    glm_features_in_trial.m: include pupil diameter as a time-varying predictor
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- glm_features_in_trial.m
+++ glm_features_in_trial.m
@@ -1,4 +1,4 @@
-function [stim_muae_zs, predictors] = glm_features_in_trial(muae_zs, times_in_trial, stims_in_trial)
+function [stim_muae_zs, predictors] = glm_features_in_trial(muae_zs, pupils, times_in_trial, stims_in_trial)
 %GLM_FEATURES
 % stim_muae_zs: Stimuli x Samples in Time x Trials
 % predictors: Stimuli x Samples in Time x Trials x Features
@@ -8,7 +8,7 @@ stim_indices = stim_time_indices(times_in_trial, stims_in_trial);
 ntimes = round(mean(stim_indices(:, 2) - stim_indices(:, 1)));
 
 stim_muae_zs = nan(nstims, ntimes, ntrials);
-predictors = nan(nstims, ntimes, ntrials, 4);
+predictors = nan(nstims, ntimes, ntrials, 5);
 for s=1:nstims
     last_idx = stim_indices(s, 1) + ntimes - 1;
     stim_muae_zs(s, :, :) = mean(muae_zs(:, stim_indices(s, 1):last_idx, :), 1);
@@ -21,7 +21,8 @@ for s=1:nstims
 
     stim_times_in_trial = permute(times_in_trial(:, stim_indices(s, 1):last_idx), [3, 1, 2]);
     predictors(s, :, :, 1) = repmat(stim_times_in_trial, [1, 1, 1, ntrials]);
-    predictors(s, :, :, 2:end) = predictor;
+    predictors(s, :, :, 2) = pupils(:, stim_indices(s, 1):last_idx, :);
+    predictors(s, :, :, 3:end) = predictor;
 end
 end
 
