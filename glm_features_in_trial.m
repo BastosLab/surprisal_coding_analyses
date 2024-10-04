b5342046ce010f62177b7482113390aa53dcc6e9
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-04 12:16:52 -0500

    glm_features_in_trial: remove one feature and document matrix dimensions
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- glm_features_in_trial.m
+++ glm_features_in_trial.m
@@ -2,21 +2,21 @@ function [stim_muae_zs, predictors] = glm_features_in_trial(muae_zs, pupils, tim
 %GLM_FEATURES
 % stim_muae_zs: Stimuli x Samples in Time x Trials
 % predictors: Stimuli x Samples in Time x Trials x Features
+% Features = [Time in trial, Pupil z-score, Trial, Stimulus Count/Index];
 ntrials = size(muae_zs, 3);
 nstims = size(stims_in_trial, 1);
 stim_indices = stim_time_indices(times_in_trial, stims_in_trial);
 ntimes = round(mean(stim_indices(:, 2) - stim_indices(:, 1)));
 
 stim_muae_zs = nan(nstims, ntimes, ntrials);
-predictors = nan(nstims, ntimes, ntrials, 5);
+predictors = nan(nstims, ntimes, ntrials, 4);
 for s=1:nstims
     last_idx = stim_indices(s, 1) + ntimes - 1;
     stim_muae_zs(s, :, :) = mean(muae_zs(:, stim_indices(s, 1):last_idx, :), 1);
 
-    predictor = zeros(ntrials, 3);
+    predictor = zeros(ntrials, 2);
     predictor(:, 1) = 1:ntrials;
-    predictor(:, 2) = squeeze(mean(stims_in_trial(s, :), 2));
-    predictor(:, 3) = s;
+    predictor(:, 2) = s;
     predictor = repmat(permute(predictor, [4, 3, 1, 2]), [1, ntimes, 1, 1]);
 
     stim_times_in_trial = permute(times_in_trial(:, stim_indices(s, 1):last_idx), [3, 1, 2]);
