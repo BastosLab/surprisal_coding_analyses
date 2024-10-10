a99a1bff0fd1482836f2ad7078669b94ea716740
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-10 16:00:06 -0500

    longitudinal_regression: analyze all four stimuli in each trial's sequence
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- longitudinal_regression.m
+++ longitudinal_regression.m
@@ -2,19 +2,17 @@ function [beta,Sigma,E,CovB,logL] = longitudinal_regression(stim_muae_zs, predic
 %LONGITUDINAL_REGRESSION
 % Response matrix [Samples in Time x Trials]
 % Design matrices length-(Samples in Time) cell-array of Trials x Features
-onset_muae_zs = squeeze(stim_muae_zs(1, :, :));
-onset_predictors = squeeze(predictors(1, :, :, :));
-oddball_muae_zs = squeeze(stim_muae_zs(4, :, :));
-oddball_predictors = squeeze(predictors(4, :, :, :));
+muae_zs = cat(1, ...
+    cat(1, squeeze(stim_muae_zs(1, :, :)), squeeze(stim_muae_zs(2, :, :))), ...
+    cat(1, squeeze(stim_muae_zs(3, :, :)), squeeze(stim_muae_zs(4, :, :))));
 
-muae_zs = cat(1, onset_muae_zs, oddball_muae_zs);
+predictors = cat(1, ...
+    cat(1, squeeze(predictors(1, :, :, :)), squeeze(predictors(2, :, :, :))), ...
+    cat(1, squeeze(predictors(3, :, :, :)), squeeze(predictors(4, :, :, :))));
+
 temporal_predictors = cell(size(muae_zs, 1), 1);
-for t=1:size(onset_predictors, 1)
-    temporal_predictors{t} = cat(2, ones(size(muae_zs, 2), 1), squeeze(onset_predictors(t, :, :)));
-end
-for t=1:size(oddball_predictors, 1)
-    s = t + size(onset_predictors, 1);
-    temporal_predictors{s} = cat(2, ones(size(muae_zs, 2), 1), squeeze(oddball_predictors(t, :, :)));
+for t=1:size(predictors, 1)
+    temporal_predictors{t} = cat(2, ones(size(muae_zs, 2), 1), squeeze(predictors(t, :, :)));
 end
 
 [beta,Sigma,E,CovB,logL] = mvregress(temporal_predictors, muae_zs, 'algorithm', 'cwls');
