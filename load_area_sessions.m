8c4fd80f50337f8c82b6809423c57669a1a7b049
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-10 17:37:03 -0500

    load_area_sessions: load, concatenate MUAe data across multiple sessions
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

---
+++ load_area_sessions.m
@@ -0,0 +1,23 @@
+function [stim_muae_zs,predictors] = load_area_sessions(sessions_path)
+%LOAD_AREA_SESSIONS Summary of this function goes here
+%   Detailed explanation goes here
+sessions = load(sessions_path, 'SESSIONS');
+sessions = sessions.SESSIONS;
+stim_muae_zs = nan;
+predictors = nan;
+for s=1:size(sessions, 1)
+    nwb = nwbRead(sessions{s, 1}, 'ignorecache');
+    stim_probs = load(sessions{s, 2}, 'stim_probs');
+    stim_probs = stim_probs.stim_probs;
+
+    [smz, ps] = glm_features(nwb, 0.5, 0.5, stim_probs);
+    if isnan(stim_muae_zs)
+        stim_muae_zs = smz;
+        predictors = ps;
+    else
+        stim_muae_zs = cat(3, stim_muae_zs, smz);
+        predictors = cat(3, predictors, ps);
+    end
+end
+end
+
