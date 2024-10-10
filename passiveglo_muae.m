5ea51eb1496875456b60d764f0374d074c49ee5e
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-10 15:31:48 -0500

    passiveglo_muae: retrieve raw MUAe data, w/o baseline and z-score
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- passiveglo_muae.m
+++ passiveglo_muae.m
@@ -1,11 +1,11 @@
-function [muae_zs, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, baseline, p)
+function [muae, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, p)
 %CHANNEL_TIME_FEATURES
-%muaez_zs: Channels x Samples in Time x Trials
+%muae: Channels x Samples in Time x Trials
 %times_in_trial: 1 x Samples in Time
 %stims_in_trial: Stimuli x 2 Times
 
 stims_in_trial = epoch_stimtimes(trials, stims);
-probe0_data = nwb.acquisition.get(['probe_', int2str(p), '_muae']).electricalseries.get(['probe_', int2str(p), '_muae_data']);
-[muae_zs, times_in_trial] = muae_from_times(probe0_data, trials, baseline);
+probe_data = nwb.acquisition.get(['probe_', int2str(p), '_muae']).electricalseries.get(['probe_', int2str(p), '_muae_data']);
+[muae, times_in_trial] = epoch_from_times(probe_data, trials);
 end
 
