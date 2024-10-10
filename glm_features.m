41314097f08e0bc9c3d16ea11e39db193a4d1a18
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-10 15:33:15 -0500

    glm_features: retrieve raw MUAe microvoltages, w/o baselining, z-scoring
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- glm_features.m
+++ glm_features.m
@@ -1,6 +1,6 @@
-function [stim_muae_zs, predictors] = glm_features(nwb, baseline, offset, stim_probs, p)
+function [stim_muae, predictors] = glm_features(nwb, offset, stim_probs, p)
 %GLM_FEATURES
-%stim_muae_zs: Stimuli x Samples in Time x Trials
+%stim_muae: Stimuli x Samples in Time x Trials
 %predictors: Stimuli x Samples in Time x Trials x Features
 % Features = [Time in session, Pupil z-score, Trial, Stimulus Count/Index,
 %             Oddball, Orientation, Sequence type (block),
@@ -10,8 +10,8 @@ function [stim_muae_zs, predictors] = glm_features(nwb, baseline, offset, stim_p
 trials(:, 1) = trials(:, 1) - offset;
 trials(:, 2) = trials(:, 2) + offset;
 pupils = time_zscore(epoch_signal(nwb, 'pupil', trials));
-[muae_zs, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, baseline, p);
-[stim_muae_zs, predictors] = glm_features_in_trial(muae_zs, pupils, times_in_trial, stims_in_trial);
+[muae, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, p);
+[stim_muae, predictors] = glm_features_in_trial(muae, pupils, times_in_trial, stims_in_trial);
 predictors(:, :, :, 1) = predictors(:, :, :, 1) + repmat(permute(stims(:, :, 1), [2, 3, 1, 4]), [1, size(predictors, 2), 1]);
 predictors = cat(4, predictors, repmat(permute(stim_info, [2, 4, 1, 3]), [1, size(predictors, 2), 1, 1]));
 end
