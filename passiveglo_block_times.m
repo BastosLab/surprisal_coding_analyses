4b4c147aaef5e4f9c2757eceb845e2f06e51609d
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-10 13:26:27 -0500

    passiveglo_block_times: calculate marginal stimuli probabilities across history
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- passiveglo_block_times.m
+++ passiveglo_block_times.m
@@ -1,13 +1,21 @@
-function [trial_times, stim_times, stim_info] = passiveglo_block_times(nwb, intervals_mask, stim_probs)
+function [trial_times, stim_times, stim_info] = passiveglo_block_times(nwb, stim_probs)
 %EPOCH_PASSIVEGLO_BLOCK
 % trial_times: Trials x 2
 % stim_times: Trials x Stimuli x 2
 % stim_info: Trials x Stimuli x Features ([Oddball, Orientation, Sequence type (block), Surprisal])
 intervals = nwb.intervals.get('passive_glo').vectordata;
+correct_intervals = intervals.get('correct').data(:);
 block_intervals = strcmp(intervals.get('sequence_type').data(:), 'gloexp');
 block_intervals = block_intervals | strcmp(intervals.get('sequence_type').data(:), 'rndctl');
 block_intervals = block_intervals | strcmp(intervals.get('sequence_type').data(:), 'seqctl');
-block_intervals = block_intervals & logical(intervals_mask);
+block_intervals = block_intervals & logical(correct_intervals);
+
+count_a = cumsum(intervals.get('orientation').data(:) == 45);
+count_b = cumsum(intervals.get('orientation').data(:) == 135);
+count_nan = cumsum(isnan(intervals.get('orientation').data(:)));
+marginalA = count_a ./ ((1:size(count_a, 1))' - count_nan);
+marginalB = count_b ./ ((1:size(count_b, 1))' - count_nan);
+marginals = cat(2, marginalA, marginalB);
 
 interval_trial_nums = intervals.get('trial_num').data(:);
 interval_stimulus_nums = intervals.get('stimulus_number').data(:);
@@ -33,23 +41,25 @@ trial_times(:, 2) = nwb.intervals.get('passive_glo').stop_time.data(trial_interv
 % block number (main = 1, rand ctl = 2, seq ctl = 3). Use those to
 % calculate further derived features.
 stim_times = nan(size(stimulus_intervals, 1), 4, 2);
-stim_info = nan(size(stimulus_intervals, 1), 4, 4);
+stim_info = nan(size(stimulus_intervals, 1), 4, 5);
 sequence_types = {'gloexp', 'rndctl', 'seqctl'};
 for s = 1:4
     stim_intervals = stimulus_intervals(:, s+1);
     stim_times(:, s, 1) = nwb.intervals.get('passive_glo').start_time.data(stim_intervals);
     stim_times(:, s, 2) = nwb.intervals.get('passive_glo').stop_time.data(stim_intervals);
 
-    stim_info(:, s, 1) = nwb.intervals.get('passive_glo').vectordata.get('oddball_status').data(stim_intervals);
-    stim_info(:, s, 2) = nwb.intervals.get('passive_glo').vectordata.get('orientation').data(stim_intervals);
-    stim_sequence_types = nwb.intervals.get('passive_glo').vectordata.get('sequence_type').data(stim_intervals);
+    stim_info(:, s, 1) = intervals.get('oddball_status').data(stim_intervals);
+    stim_info(:, s, 2) = intervals.get('orientation').data(stim_intervals);
+    stim_sequence_types = intervals.get('sequence_type').data(stim_intervals);
     for seq = 1:numel(stim_sequence_types)
         stim_info(seq, s, 3) = find(strcmp(sequence_types, stim_sequence_types(seq)));
     end
     oddball_status = stim_info(:, s, 1) + 1;
     stim_angles = (stim_info(:, s, 2) == 135) + 1;
+    stim_marginals = marginals(stim_intervals, :);
     for t = 1:numel(trial_nums)
         stim_info(t, s, 4) = -log2(stim_probs(oddball_status(t), stim_angles(t), stim_info(t, s, 3)));
+        stim_info(t, s, 5) = -log2(stim_marginals(t, stim_angles(t)));
     end
 end
 end
