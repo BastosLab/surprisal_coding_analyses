7cfb659515c08feb3215e5f43c3dd30855da2ebf
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-21 12:51:49 -0500

    glm_features: stimulus info as predictors at stimulus times in full trial
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- glm_features.m
+++ glm_features.m
@@ -9,7 +9,6 @@ function [muae, predictors] = glm_features(nwb, trials, smoothing, stim_probs, p
 [stims, stim_info] = passiveglo_block_times(nwb, stim_probs);
 pupils = time_zscore(epoch_signal(nwb, 'pupil', trials));
 [muae, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, smoothing, stims, p);
-predictors = glm_features_in_trial(muae, pupils, times_in_trial, stims_in_trial);
-predictors(:, :, :, 1) = predictors(:, :, :, 1) + repmat(permute(stims(:, :, 1), [2, 3, 1, 4]), [1, size(predictors, 2), 1]);
-predictors = cat(4, predictors, repmat(permute(stim_info, [2, 4, 1, 3]), [1, size(predictors, 2), 1, 1]));
+predictors = glm_features_in_trial(pupils, times_in_trial, stims_in_trial, stim_info);
+predictors(:, :, 1) = predictors(:, :, 1) + repmat(permute(trials(:, 1), [2, 1]), [size(predictors, 1), 1]);
 end
--- glm_features_in_trial.m
+++ glm_features_in_trial.m
@@ -1,19 +1,23 @@
-function [predictors] = glm_features_in_trial(muae, pupils, times_in_trial, stims_in_trial)
+function [predictors] = glm_features_in_trial(pupils, times_in_trial, stims_in_trial, stim_info)
 %GLM_FEATURES
 % predictors: Samples in Time x Trials x Features
-% Features = [Time in trial, Pupil z-score, Trial, Stimulus Count/Index];
-ntrials = size(muae, 3);
+% Features = [Time in trial, Pupil z-score, Trial, Stimulus Count/Index,
+% Oddball, Orientation, Sequence type (block), Conditional Surprisal,
+% Marginal Surprisal, Cumulative Conditional Surprisal,
+% Cumulative Marginal Surprisal];
+ntrials = size(stim_info, 1);
 nstims = size(stims_in_trial, 1);
 stim_indices = stim_time_indices(times_in_trial, stims_in_trial);
-stim_length = round(mean(stim_indices(:, 2) - stim_indices(:, 1)));
-ntimes = size(muae_zs, 2);
+ntimes = size(times_in_trial, 2);
 
-predictors = nan(ntimes, ntrials, 4);
+predictors = zeros(ntimes, ntrials, 11);
 predictors(:, :, 1) = repmat(permute(times_in_trial, [2, 1]), [1, ntrials]);
-predictors(:, :, 2) = pupils;
-predictors(:, :, 3) = 1:ntrials;
+predictors(:, :, 2) = pupils(:, 1:ntimes, :);
+predictors(:, :, 3) = repmat(1:ntrials, [ntimes, 1]);
 for s=1:nstims
     predictors(stim_indices(s, 1):stim_indices(s, 2), :, 4) = s;
+    stimulus_predictors = repmat(permute(stim_info(:, s, :), [2, 1, 3]), [stim_indices(s, 2) - stim_indices(s, 1) + 1, 1, 1]);
+    predictors(stim_indices(s, 1):stim_indices(s, 2), :, 5:11) = stimulus_predictors;
 end
 end
 
