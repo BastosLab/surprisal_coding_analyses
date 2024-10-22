1c897cc70a89d26eac597adc3da31279ce28bd68
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-22 12:36:19 -0500

    glm_features: pass in probe number as an argument
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- glm_features.m
+++ glm_features.m
@@ -1,15 +1,16 @@
-function [stim_muae_zs, predictors] = glm_features(nwb, baseline, offset, stim_probs)
+function [stim_muae_zs, predictors] = glm_features(nwb, baseline, offset, stim_probs, p)
 %GLM_FEATURES
 %stim_muae_zs: Stimuli x Samples in Time x Trials
 %predictors: Stimuli x Samples in Time x Trials x Features
 % Features = [Time in session, Pupil z-score, Trial, Stimulus Count/Index,
 %             Oddball, Orientation, Sequence type (block),
-%             Conditional Surprisal, Marginal Surprisal];
+%             Conditional Surprisal, Marginal Surprisal, Cumulative Conditional Surprisal,
+%             Cumulative Marginal Surprisal];
 [trials, stims, stim_info] = passiveglo_block_times(nwb, stim_probs);
 trials(:, 1) = trials(:, 1) - offset;
 trials(:, 2) = trials(:, 2) + offset;
 pupils = time_zscore(epoch_signal(nwb, 'pupil', trials));
-[muae_zs, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, baseline);
+[muae_zs, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, baseline, p);
 [stim_muae_zs, predictors] = glm_features_in_trial(muae_zs, pupils, times_in_trial, stims_in_trial);
 predictors(:, :, :, 1) = predictors(:, :, :, 1) + repmat(permute(stims(:, :, 1), [2, 3, 1, 4]), [1, size(predictors, 2), 1]);
 predictors = cat(4, predictors, repmat(permute(stim_info, [2, 4, 1, 3]), [1, size(predictors, 2), 1, 1]));
