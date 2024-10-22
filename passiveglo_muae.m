18a762d23911edd6d5121f28b8b4607a8377e537
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-22 12:36:03 -0500

    passiveglo_muae: pass in probe number as an argument
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- passiveglo_muae.m
+++ passiveglo_muae.m
@@ -1,11 +1,11 @@
-function [muae_zs, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, baseline)
+function [muae_zs, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, baseline, p)
 %CHANNEL_TIME_FEATURES
 %muaez_zs: Channels x Samples in Time x Trials
 %times_in_trial: 1 x Samples in Time
 %stims_in_trial: Stimuli x 2 Times
 
 stims_in_trial = epoch_stimtimes(trials, stims);
-probe0_data = nwb.acquisition.get('probe_0_muae').electricalseries.get('probe_0_muae_data');
+probe0_data = nwb.acquisition.get(['probe_', int2str(p), '_muae']).electricalseries.get(['probe_', int2str(p), '_muae_data']);
 [muae_zs, times_in_trial] = muae_from_times(probe0_data, trials, baseline);
 end
 
