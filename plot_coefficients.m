7661ba73c97f6d098ae519df1ea646f1e22962e6
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-22 12:36:55 -0500

    plot_coefficients.m: plot regression coefficients with 99% confidence interval
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

---
+++ plot_coefficients.m
@@ -0,0 +1,16 @@
+function [] = plot_coefficients(betas,std_devs)
+%PLOT_COEFFICIENTS Summary of this function goes here
+%   Detailed explanation goes here
+xs = 1:size(betas, 1);
+bar(xs, betas);
+hold on;
+er = errorbar(xs, betas, 2.576 * std_devs, 2.576 * std_devs);
+er.Color = [0 0 0];
+er.LineStyle = 'none';
+xticklabels({'Y-intercept', 'Time in session', 'Pupil width', 'Trial', ...
+    'Stimulus Index', 'Oddball', 'Orientation', ...
+    'Block type', 'Conditional Surprisal', 'Marginal Surprisal', ...
+    'Cumulative Conditional Surprisal', 'Cumulative Marginal Surprisal'});
+hold off;
+end
+
