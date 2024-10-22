2cdff729af3ade86c7c4057974b7a247e0b4af29
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-22 12:36:35 -0500

    load_area_sessions: pass in probe number as an argument
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- load_area_sessions.m
+++ load_area_sessions.m
@@ -1,4 +1,4 @@
-function [stim_muae_zs,predictors] = load_area_sessions(sessions_path)
+function [stim_muae_zs,predictors] = load_area_sessions(sessions_path, probe)
 %LOAD_AREA_SESSIONS Summary of this function goes here
 %   Detailed explanation goes here
 sessions = load(sessions_path, 'SESSIONS');
@@ -10,7 +10,7 @@ for s=1:size(sessions, 1)
     stim_probs = load(sessions{s, 2}, 'stim_probs');
     stim_probs = stim_probs.stim_probs;
 
-    [smz, ps] = glm_features(nwb, 0.5, 0.5, stim_probs);
+    [smz, ps] = glm_features(nwb, 0.5, 0.5, stim_probs, probe);
     if isnan(stim_muae_zs)
         stim_muae_zs = smz;
         predictors = ps;
