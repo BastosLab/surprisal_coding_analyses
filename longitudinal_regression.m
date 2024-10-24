677c4c33f63dd15d0d429918226426811eff159e
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-24 16:10:27 -0500

    longitudinal_regression: conduct regression over whole trial, not just stimuli
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- longitudinal_regression.m
+++ longitudinal_regression.m
@@ -1,15 +1,7 @@
-function [beta,Sigma,E,CovB,logL] = longitudinal_regression(stim_muae, predictors)
+function [beta,Sigma,E,CovB,logL] = longitudinal_regression(muae, predictors)
 %LONGITUDINAL_REGRESSION
 % Response matrix [Samples in Time x Trials]
 % Design matrices length-(Samples in Time) cell-array of Trials x Features
-muae = cat(1, ...
-    cat(1, squeeze(stim_muae(1, :, :)), squeeze(stim_muae(2, :, :))), ...
-    cat(1, squeeze(stim_muae(3, :, :)), squeeze(stim_muae(4, :, :))));
-
-predictors = cat(1, ...
-    cat(1, squeeze(predictors(1, :, :, :)), squeeze(predictors(2, :, :, :))), ...
-    cat(1, squeeze(predictors(3, :, :, :)), squeeze(predictors(4, :, :, :))));
-
 temporal_predictors = cell(size(muae, 1), 1);
 for t=1:size(predictors, 1)
     temporal_predictors{t} = cat(2, ones(size(muae, 2), 1), squeeze(predictors(t, :, :)));
