a34ecece3196fdcfe54303b6cc42e6f99a2fdb59
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-24 11:32:49 -0500

    passiveglo_muae: smooth MUAe with Gaussian kernel of specified width
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- passiveglo_muae.m
+++ passiveglo_muae.m
@@ -1,4 +1,4 @@
-function [muae, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, stims, p)
+function [muae, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, smoothing, stims, p)
 %CHANNEL_TIME_FEATURES
 %muae: Channels x Samples in Time x Trials
 %times_in_trial: 1 x Samples in Time
@@ -6,6 +6,7 @@ function [muae, times_in_trial, stims_in_trial] = passiveglo_muae(nwb, trials, s
 
 stims_in_trial = epoch_stimtimes(trials, stims);
 probe_data = nwb.acquisition.get(['probe_', int2str(p), '_muae']).electricalseries.get(['probe_', int2str(p), '_muae_data']);
-[muae, times_in_trial] = epoch_from_times(probe_data, trials);
+[muae, times_in_trial] = epoch_from_times(probe_data, trials, smoothing);
+
 end
 
