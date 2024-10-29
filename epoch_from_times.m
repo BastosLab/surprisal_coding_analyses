00bfa09e818dcf4c9dc887b4baf4744f064cc90a
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-29 13:12:26 -0500

    epoch_from_times: make sure end of vrange is integer
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epoch_from_times.m
+++ epoch_from_times.m
@@ -3,7 +3,7 @@ function [data, times] = epoch_from_times(probe_signal,trial_times,smoothing)
 %   Detailed explanation goes here
 freq = probe_signal.starting_time_rate;
 idxs = nearest_index(probe_signal.timestamps(:), trial_times(:, 1));
-vrange = [0, mean(trial_times(:, 2) - trial_times(:, 1)) * freq];
+vrange = [0, int64(mean(trial_times(:, 2) - trial_times(:, 1)) * freq)];
 data = epoch_data(probe_signal.data, idxs, vrange);
 times = 0:(1/freq):(size(data, 2) / freq);
 times = times(:, 1:size(data, 2));
